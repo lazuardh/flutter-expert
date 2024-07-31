4a6aa41479f5fdff66e12aef777ea0e65c9992b8
lazuardh <habiyanlazuard21@gmail.com>
2024-07-31 23:55:10 +0700

    Remove obsolete test files

--- about/test/about_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:core/styles/colors.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:about/about.dart';
-
-void main() {}
