99a6a630e2607be6d3199ff983b0a711883455a8
lazuardh <habiyanlazuard21@gmail.com>
2024-07-31 20:52:08 +0700

    added _test in main project

--- about/test/about_test.dart
+++ about/test/about_test.dart
@@ -1,3 +1,7 @@
+import 'package:core/styles/colors.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:about/about.dart';
+
+void main() {}
